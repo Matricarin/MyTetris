9e7b8155141dedfca7d9b250e863a46a2881da51
Matricarin <matricarin@gmail.com>
2022-06-18 17:47:24 +0500

    Create new Project. My first Commit!

---
+++ MyTetris/MyTetris/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyTetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Work with GitHub!");
+
+            Console.ReadKey();
+        }
+    }
+}
