fb3539a6df7d55842da4ff226f5a12b845804f44
Matricarin <matricarin@gmail.com>
2022-06-18 19:54:11 +0500

    New project for collections

---
+++ MyTetris/Collections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MyTetris;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(50, 50);
+            Console.SetBufferSize(50, 50);
+
+            Point[] points = new Point[3];
+            points[0] = new Point(3, 3, '#');
+            points[1] = new Point(3, 5, '#');
+            points[2] = new Point(3, 7, '#');
+
+            foreach (Point p in points)
+            {
+                p.Draw();
+            }
+            DrawSquare(5, 5, '#');
+
+            Console.ReadKey();
+        }
+        static void DrawSquare(int x, int y, char c)
+        {
+            Point[][] square = new Point[2][];
+            square[0] = new Point[2];
+            square[1] = new Point[2];
+            square[0][0] = new Point(x, y, c);
+            square[0][1] = new Point(x, y + 1, c);
+            square[1][0] = new Point(x + 1, y, c);
+            square[1][1] = new Point(x + 1, y + 1, c);
+            for (int i = 0; i < square.Length; i++)
+            {
+                for (int j = 0; j < square[i].Length; j++)
+                {
+                    square[i][j].Draw();
+                }
+            }
+        }
+    }
+}
