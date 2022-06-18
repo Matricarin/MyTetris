5320774b81f82175f78295cbda68a48215bc2eac
Matricarin <matricarin@gmail.com>
2022-06-18 20:26:05 +0500

    Comment

--- MyTetris/Collections/Program.cs
+++ MyTetris/Collections/Program.cs
@@ -19,26 +19,26 @@ namespace Collections
             {
                 p.Draw();
             }
-            DrawSquare(5, 5, '#');
+        //    DrawSquare(5, 5, '#');
 
             Console.ReadKey();
         }
-        static void DrawSquare(int x, int y, char c)
-        {
-            Point[][] square = new Point[2][];
-            square[0] = new Point[2];
-            square[1] = new Point[2];
-            square[0][0] = new Point(x, y, c);
-            square[0][1] = new Point(x, y + 1, c);
-            square[1][0] = new Point(x + 1, y, c);
-            square[1][1] = new Point(x + 1, y + 1, c);
-            for (int i = 0; i < square.Length; i++)
-            {
-                for (int j = 0; j < square[i].Length; j++)
-                {
-                    square[i][j].Draw();
-                }
-            }
+        //static void DrawSquare(int x, int y, char c)
+        //{
+        //    Point[][] square = new Point[2][];
+        //    square[0] = new Point[2];
+        //    square[1] = new Point[2];
+        //    square[0][0] = new Point(x, y, c);
+        //    square[0][1] = new Point(x, y + 1, c);
+        //    square[1][0] = new Point(x + 1, y, c);
+        //    square[1][1] = new Point(x + 1, y + 1, c);
+        //    for (int i = 0; i < square.Length; i++)
+        //    {
+        //        for (int j = 0; j < square[i].Length; j++)
+        //        {
+        //            square[i][j].Draw();
+        //        }
+        //    }
         }
     }
 }
