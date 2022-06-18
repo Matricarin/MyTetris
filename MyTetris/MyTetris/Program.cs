b36213f273ea4027ee9faf75646f91edce28ece8
Matricarin <matricarin@gmail.com>
2022-06-18 18:05:25 +0500

    Create Method Draw.

--- MyTetris/MyTetris/Program.cs
+++ MyTetris/MyTetris/Program.cs
@@ -7,8 +7,20 @@ namespace MyTetris
         static void Main(string[] args)
         {
             Console.WriteLine("Work with GitHub!");
+            Console.SetWindowSize(50, 40);
+            Console.SetBufferSize(50, 40);
+            int x1 = 5;
+            int y1 = 10;
+            char c1 = '#';
 
+            Draw(x1, y1, c1);
             Console.ReadKey();
+        }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
