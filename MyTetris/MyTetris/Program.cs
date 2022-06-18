9b083ff035e02b25b04612ba9af1fd1abb2e8deb
Matricarin <matricarin@gmail.com>
2022-06-18 20:25:44 +0500

    Draw 1 point Draw 1 square

--- MyTetris/MyTetris/Program.cs
+++ MyTetris/MyTetris/Program.cs
@@ -9,22 +9,16 @@ namespace MyTetris
             Console.WriteLine("Work with GitHub!");
             Console.SetWindowSize(50, 40);
             Console.SetBufferSize(50, 40);
-            
-            Point p1 = new Point();
-            p1.X = 5;
-            p1.Y = 10;
-            p1.Sym = '#';
-            p1.Draw();
 
-            Point p2 = new Point(3, 9, '#');
-            p2.Draw();
-            Point p3 = new Point()
-            {
-                X = 5,
-                Y = 8,
-                Sym = '#'
-            };
-            p3.Draw();
+            Console.WriteLine("Start Point: ");
+            Point p1 = new Point(14, 1, '#');
+            p1.Draw();
+            Console.WriteLine();
+            Console.WriteLine("Start Square: ");
+            Square s1 = new Square(14, 2, '#');
+            s1.Draw();
+            Console.WriteLine();
+
 
             Console.ReadKey();
         }               
