03d196fd609ae0a138b37928d51d894515a70410
Matricarin <matricarin@gmail.com>
2022-06-23 19:08:20 +0500

    ref and out.

--- MyTetris/MyTetris/Program.cs
+++ MyTetris/MyTetris/Program.cs
@@ -11,23 +11,26 @@ namespace MyTetris
             Console.SetWindowSize(50, 40);
             Console.SetBufferSize(50, 40);
             FigureGenerator generator = new FigureGenerator(25, 0, '#');
-            Figure fig;
+            Figure s = null;
             while (true)
             {
-                fig = generator.GetNewFigure();
-                fig.Draw();
-                for (int i = 0; i < 15; i++)
-                {
-                    Thread.Sleep(200);
-                    fig.Hide();
-                    fig.Move(Direction.Down);
-                    fig.Draw();
-                }
-
+                
+                FigureFall(out s, generator);
+                s.Draw();
             }
-
-
             Console.ReadKey();
-        }        
+        }
+        static void FigureFall(out Figure fig, FigureGenerator generator)
+        {
+            fig = generator.GetNewFigure();
+            fig.Draw();
+            for (int i = 0; i < 15; i++)
+            {
+                Thread.Sleep(200);
+                fig.Hide();
+                fig.Move(Direction.Down);
+                fig.Draw();
+            }
+        }
     }
 }
